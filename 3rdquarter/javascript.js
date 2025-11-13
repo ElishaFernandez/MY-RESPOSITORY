24b13c779ab203d80011db48609d6ecc26d7590e
ElishaFernandez <fernandezelisha19@gmail.com>
2025-11-13 21:04:35 +0800

    updated

--- 3rdquarter/javascript.js
+++ 3rdquarter/javascript.js
@@ -1,2 +1,10 @@
-document.getElementById("jsexternal").innerHTML = "Hello World JavaScript in the Head";
-window.alert("Welcome to JavaScript!");
+function changep3() {
+    document.getElementById("p3").innerHTML = "Paragraph 3 changed by JavaScript in the EXTERNAL of HTML";
+  }
+  
+function alert3() {
+  alert("This is alert 3 from javascript in the body of HTML");
+}
+function console3() {
+  console.log("This is console log 3 from javascript in the body of HTML");
+}
